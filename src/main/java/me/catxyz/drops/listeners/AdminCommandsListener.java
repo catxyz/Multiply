82c8b83030ae6249fc5fde3f684a8d48829f01d2
catxyz <69825595+catxyz@users.noreply.github.com>
2022-04-14 22:54:09 +0200

    Updated admin command processing

--- src/main/java/me/catxyz/drops/listeners/AdminCommandsListener.java
+++ src/main/java/me/catxyz/drops/listeners/AdminCommandsListener.java
@@ -5,6 +5,7 @@ import me.catxyz.drops.managers.ItemManager;
 import me.catxyz.drops.utils.Text;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -18,32 +19,37 @@ public record AdminCommandsListener(ItemManager itemManager) implements Listener
         Player player = event.getPlayer();
         String message = event.getMessage();
 
-        String command = "/multiplier ";
-        if (message.startsWith(command)) {
+        if (message.startsWith("/multiplier ")) {
             if (player.hasPermission("drops.multiplier")) {
-                if (message.startsWith(command + "clear")) {
-                    Map<Material, Integer> items = Maps.newHashMap(itemManager.getItems());
-                    items.forEach((material, multiplier) -> {
-                        player.sendMessage(
-                                Text.format("&cCleared multiplier for &6" + material)
-                        );
-                        itemManager.removeItem(material);
-                    });
-                    event.setCancelled(true);
-                }
-                if (message.startsWith(command + "list")) {
-                    if (!itemManager.getItems().isEmpty()) {
-                        player.sendMessage(Text.format("&eItem List:"));
-                        itemManager.getItems().forEach((material, multiplier) -> player.sendMessage(
-                                Text.format(" &8• &6" + material + " &8- &a" + multiplier + "&bx")
-                        ));
-                    } else {
-                        player.sendMessage(Text.format("&cItem list is empty."));
-                    }
-                    event.setCancelled(true);
-                }
+                processCommands(player, message, event);
             } else {
                 player.sendMessage(Text.format("&cCould not modify this."));
+            }
+        }
+    }
+
+    private void processCommands(Player player, String command, Cancellable cancellable) {
+        switch (command) {
+            case "/multiplier clear" -> {
+                Map<Material, Integer> items = Maps.newHashMap(itemManager.getItems());
+                items.forEach((material, multiplier) -> {
+                    player.sendMessage(
+                            Text.format("&cCleared multiplier for &6" + material)
+                    );
+                    itemManager.removeItem(material);
+                });
+                cancellable.setCancelled(true);
+            }
+            case "/multiplier list" -> {
+                if (!itemManager.getItems().isEmpty()) {
+                    player.sendMessage(Text.format("&eItem List:"));
+                    itemManager.getItems().forEach((material, multiplier) -> player.sendMessage(
+                            Text.format(" &8• &6" + material + " &8- &a" + multiplier + "&bx")
+                    ));
+                } else {
+                    player.sendMessage(Text.format("&cItem list is empty."));
+                }
+                cancellable.setCancelled(true);
             }
         }
     }
